ce2dbe28343da17f8727335c4cbdc4fa2defc991
Slavik Manukyan <slavikmanukyan@gmail.com>
2018-01-29 19:50:34 +0400

    upgrade dependencies

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/main.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 
 import {bootstrap} from 'angular2/platform/browser';
 import {Component, provide} from 'angular2/core';
